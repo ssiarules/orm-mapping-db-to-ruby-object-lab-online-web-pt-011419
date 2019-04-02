3b250ed3e0ea4f6e72824f175f79e9c0b4a2f667
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-02 06:06:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ end
 end 
   def self.first_X_students_in_grade_10(number_of_students)
     sql = <<-SQL
-    SELECT * FROM students WHERE grade = 10 ORDER BY id LIMIT ? 
+    SELECT * FROM students WHERE grade = 10 LIMIT ? 
      SQL
      DB[:conn].execute(sql,number_of_students).map do |row|
       self.new_from_db(row)
