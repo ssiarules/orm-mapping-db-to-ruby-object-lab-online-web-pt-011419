e78642ad8459a636e5955878124b273be67ab801
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-02 05:42:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ end
   
   def self.all_students_in_grade_9
     sql = <<-SQL
-    SELECT * FROM students WHERE grade = ? 
+    SELECT * FROM students WHERE grade = 9 
      SQL
      DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
