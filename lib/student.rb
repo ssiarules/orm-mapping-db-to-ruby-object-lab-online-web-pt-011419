1f7793dc6d32b779b79c86187d2aecb4ca8d103a
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-02 05:46:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ end
 
   def self.students_below_12th_grade
     sql = <<-SQL
-    SELECT * FROM students WHERE grade > 12
+    SELECT * FROM students WHERE grade < 12
      SQL
      DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
