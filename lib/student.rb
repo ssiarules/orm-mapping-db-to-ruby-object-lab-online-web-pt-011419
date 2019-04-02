c104e65cc6149eaa449a8c4867f42c17c75032fb
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-02 05:50:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ end
     sql = <<-SQL
     SELECT * FROM students WHERE grade = ?
      SQL
-     DB[:conn].execute(sql, grade).map do |row|
+     DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
   end 
     
